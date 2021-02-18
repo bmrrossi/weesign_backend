4b4b4794d3b2d1b8e180e3bcd60d3c521cb8a83f
Bruno Michel Rossi <bruno@weecode.com.br>
2021-02-18 17:16:57 -0300

    First commit

---
+++ src/main/java/br/com/weecode/weesign_backend/WeesignBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.weecode.weesign_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeesignBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeesignBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weecode/weesign_backend/WeesignBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weecode.weesign_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeesignBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
